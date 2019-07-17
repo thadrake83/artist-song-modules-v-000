3e1f96247f18e2264fb6b4005b50553581022fa8
Drake Rentrop <drakerentrop@gmail.com>
2019-07-17 20:17:11 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,3 @@ require 'pry'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
-module Memorable
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
-end
