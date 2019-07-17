828e1fb50508d6fa9d2f37e000e2c892980c81bc
Drake Rentrop <drakerentrop@gmail.com>
2019-07-17 20:48:05 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,4 +1,5 @@
 module Memorable
+  module ClassMethods
 
   def reset_all
     self.all.clear
@@ -7,5 +8,13 @@ module Memorable
   def count
     self.all.count
   end
+end
+
+module InstanceMethods
+  
+  def initialize
+
+  end
+end
 
 end
